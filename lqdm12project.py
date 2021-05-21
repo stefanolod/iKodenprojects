f32ee966f75cbe37546d4900913b5bd64964bff7
panameral <koroa@outlook.it>
2021-05-21 11:18:42 +0200

    this is my version of your idea

--- lqdm12project.py
+++ lqdm12project.py
@@ -1,17 +1,32 @@
-Advanced Calculator lqdm12 basic.py
-num1 = input("Enter a number:")
-num2 = input("Enter a number:")
-result = float(num1) + float(num2)
-print(result)
+#Advanced Calculator lqdm12 basic.py
+#I take as a string all of your calculation
+string_calculate = input("Write your calculation: ")
+num1 = ''
+num2 = ''
+op = ''
+
+#With this operation I'm going to separate the first number, the operator sign and the second number in three different variables
+for i in range(len(string_calculate)):
+  if string_calculate[i] == '+' or string_calculate[i] == '-' or string_calculate[i] == '*' or string_calculate[i] == '/':
+    op = string_calculate[i]
+  else:
+    if op == '':
+      num1 += string_calculate[i]
+    else:
+      num2 += string_calculate[i]
 
 if op == "+":
-  print(num1 + num2)
-elif op == "_":
-  print(num1 - num2)
-elif op == "/":
-  print(num1 / num2)
+  print(float(num1) + float(num2))
+elif op == "-":
+  print(float(num1) - float(num2))
 elif op == "*":
-  print(num1 * num2)
+  print(float(num1) * float(num2))
+elif op == "/":
+  #Here I try to catch the error, because when every program has a division by zero will crash (math says it's impossible to calculate)
+  try:
+    print(float(num1) / float(num2))
+  except ZeroDivisionError:
+    print("You can't divide by zero")
 else:
   print("Invalid operator")
 
